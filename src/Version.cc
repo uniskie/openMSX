010f4529e184115022cd10ed969dc22b81caa0c5
uniskie <uniuniunih@gmail.com>
2024-10-19 06:26:13 +0900

    fix Version Text error
    バージョン文字列を書き換えたことで
    Catapultからのバージョンチェックでエラーがでたので修正

--- src/Version.cc
+++ src/Version.cc
@@ -7,12 +7,9 @@ namespace openmsx {
 
 std::string Version::full()
 {
-#if defined(FOR_MAMI)
-	std::string result = strCat("openMSX for MAMI ", VERSION);
-#else
 	std::string result = strCat("openMSX ", VERSION);
-#endif
 	if constexpr (!RELEASE) strAppend(result, '-', REVISION);
+	
 	return result;
 }
 
