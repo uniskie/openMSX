97763e60e66b0ab39b79515920ad33ea1046d939
uniskie <uniuniunih@gmail.com>
2025-02-11 09:23:44 +0900

    Improved the behavior of keys where multiple same keys exist
    ホスト側の複数のキーが同じMSXキーに割り当てられているケースで、同時押ししたあとに片方を離すと、その時点でMSX側のキーが離されたことになってしまう問題への対処

--- src/input/Keyboard.cc
+++ src/input/Keyboard.cc
@@ -724,6 +724,12 @@ Keyboard::Keyboard(MSXMotherBoard& motherBoard,
 		| (config.getChildDataAsBool("code_kana_locks", false) ? KeyInfo::CODE_MASK : 0)
 		| (config.getChildDataAsBool("graph_locks", false) ? KeyInfo::GRAPH_MASK : 0))
 {
+#if USE_KEYPRESSCNT
+	for (auto& i : cntKeyMatrix) {
+		std::ranges::fill(i,  0);
+	}
+#endif //USE_KEYPRESSCNT
+
 	std::ranges::fill(keyMatrix,     255);
 	std::ranges::fill(cmdKeyMatrix,  255);
 	std::ranges::fill(typeKeyMatrix, 255);
@@ -1161,6 +1167,13 @@ void Keyboard::updateKeyMatrix(EmuTime::param time, bool down, KeyMatrixPosition
 		return;
 	}
 	if (down) {
+	#if USE_KEYPRESSCNT
+		auto row = pos.getRow();
+		auto col = pos.getColumn();
+		assert(cntKeyMatrix[row][col] < 255); // safe check
+		cntKeyMatrix[row][col]++;
+	#endif //USE_KEYPRESSCNT
+
 		pressKeyMatrixEvent(time, pos);
 		// Keep track of the MSX modifiers.
 		// The MSX modifiers sometimes get overruled by the unicode character
@@ -1172,6 +1185,14 @@ void Keyboard::updateKeyMatrix(EmuTime::param time, bool down, KeyMatrixPosition
 			}
 		}
 	} else {
+	#if USE_KEYPRESSCNT
+		auto row = pos.getRow();
+		auto col = pos.getColumn();
+		// allow - > assert(cntKeyMatrix[row][col]);
+		if (cntKeyMatrix[row][col]) --cntKeyMatrix[row][col]; //care for overflow -> std:min(0, cntKeyMatrix[row][col]-1); 
+		if (cntKeyMatrix[row][col] > 0) return;
+	#endif //USE_KEYPRESSCNT
+
 		releaseKeyMatrixEvent(time, pos);
 		for (auto [i, mp] : enumerate(modifierPos)) {
 			if (pos == mp) {
@@ -1270,7 +1291,7 @@ bool Keyboard::processKeyEvent(EmuTime::param time, bool down, const KeyEvent& k
 			it->second = unicode;
 		} else {
 			// insert new element (in the right location)
-			lastUnicodeForScancode.emplace(it, uint32_t(ScanCode), unicode);
+			lastUnicodeForScancode.emplace(it, ScanCode, unicode);
 		}
 
 		if (unicode == 0) {
@@ -1566,6 +1587,34 @@ bool Keyboard::commonKeys(unsigned unicode1, unsigned unicode2) const
 
 void Keyboard::debug(const char* format, ...) const
 {
+#if KEYTEST //-->
+	if (keyboardSettings.getTraceKeyPresses()) {
+		std::string buffer(1024, '\0');
+
+		va_list args;
+		va_start(args, format);
+		vsnprintf(buffer.data(), buffer.size(), format, args);
+		va_end(args);
+
+		// trim size;
+		auto lastpos = buffer.find('\0');
+		if (lastpos == std::string::npos) {
+			lastpos = buffer.length();
+		}
+
+		// suppress last lf
+		const std::string lf("\n");
+		auto lastlf = buffer.rfind(lf);
+		if (lastlf != std::string::npos) {
+			if ((lastlf + lf.length()) >= lastpos) {
+				lastpos = lastlf;
+			}
+		}
+
+		auto& motherBoard = keybDebuggable.getMotherBoard();
+		motherBoard.getMSXCliComm().printInfo(buffer.substr(0, lastpos));
+	}
+#else
 	if (keyboardSettings.getTraceKeyPresses()) {
 		va_list args;
 		va_start(args, format);
